506bc6b34da9a4cf95e8171e3cce584f6ec12f47
Paul Schulzetenberg <pschulzetenber@cnm.edu>
2019-03-26 10:10:38 -0600

    Updating work from videos

---
+++ variables-and-data.js
@@ -0,0 +1,45 @@
+/*
+ * Variables, Data Types, and Typing.
+ */
+
+let wordData = "Word list";
+
+console.log(wordData);
+
+/*
+let var1 = "2";
+let var2 = 3;
+console.log(var1+var2);
+*/
+
+/*
+ * Objects, Interfaces, and API's
+ */
+
+/*
+let dog = {
+	name: "Steve",
+	color: "brown",
+	breed: "Pug",
+	size: "Small",
+	bark: function(typeOfBark) {
+		console.log("Bark!");
+	}
+}
+
+dog.bark();
+*/
+
+/*
+function x(y) {
+	y.num = y.num + 5;
+	console.log(y);
+}
+
+let y = {
+	name: "Tom",
+	num: 10,
+}
+x(y);
+console.log(y);
+*/
